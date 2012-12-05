53d907009bba09c5ef71b5724417afefe6363e0f
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2012-12-05 07:04:14 +0000

    Symlink xmodmap config

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ln -sh $PWD/.vim/ ~/.vim
-ln -sh $PWD/.vimrc ~/.vimrc
-ln -sh $PWD/.gitconfig ~/.gitconfig
-ln -sh $PWD/.irssi ~/.irssi
+ln -s $PWD/.vim/ ~/.vim
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/.irssi ~/.irssi
+ln -s $PWD/.xmodmaprc ~/.xmodmaprc
