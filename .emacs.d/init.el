a223cf3a3801c5e91dd00bb207a03e11fb62a647
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-02-11 13:50:23 +0000

    Emacs: add markdown-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,7 @@
 
 (defvar my-packages '(undo-tree evil color-theme-solarized
                                 clojure-mode nrepl
+                                markdown-mode
                                 magit))
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -23,6 +24,8 @@
 (evil-mode 1)
 
 (load-theme 'solarized-light t)
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
