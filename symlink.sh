f975a3d1bef70f785bc4a2f7f9241d4631751f46
Ernestas Lisauskas <ernestas.lisauskas@gmail.com>
2013-09-28 15:45:20 +0300

    Correct xmodmap location

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,6 @@ ln -s $PWD/.vim/ ~/.vim
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.irssi ~/.irssi
-ln -s $PWD/.xmodmaprc ~/.xmodmaprc
+ln -s $PWD/.Xmodmap ~/.Xmodmap
 ln -s $PWD/.emacs.d/ ~/.emacs.d
 ln -s $PWD/.config/awesome/ ~/.config/awesome
